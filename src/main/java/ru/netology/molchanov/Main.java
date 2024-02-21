6599b351c16affa91f6156e403b2445dcf0e016c
MolchanovSergey <molchanov.sergey@htmail.com>
2024-02-21 21:17:30 +0300

    hw 2

--- src/main/java/ru/netology/molchanov/Main.java
+++ src/main/java/ru/netology/molchanov/Main.java
@@ -1,17 +1,32 @@
 package ru.netology.molchanov;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome, Sergey Molchanov!");
+        System.out.printf("Hello and welcome, Sergey Molchanov!\n");
+        System.out.printf("Please enter transaction parameters: id, amount, date.\n");
+        System.out.printf("For example: 3456723 24000,00 2024.02.24\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        int id = scanner.nextInt();
+        double sum = scanner.nextDouble();
+        String dateString = scanner.nextLine().trim();
+
+        var f = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        LocalDate date = LocalDate.parse(dateString, f);
+
+        System.out.println("You enter: " + id + " " + sum + " " + date);
     }
 }
+
+
+
